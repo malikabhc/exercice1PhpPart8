0a8af21a78ccc6a7542f4875b9356ba072d47c93
Malikabhc <malika.bouhcine@gmail.com>
2018-08-20 15:51:16 +0200

    exercice1-8

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+        <link rel="stylesheet" href="style.css" />
+        <title>Exercice1/8</title>
+    </head>
+    <body>
+            <!-- Affichage de l'en-tête User-agent -->
+            <p><?= $_SERVER['HTTP_USER_AGENT'] . ' '; ?></p>
+            <!-- Affichage de l'adresse IP -->
+            <p><?= $_SERVER['SERVER_ADDR'] . ' '; ?></p>
+            <!-- Affichage du nom du serveur hôte -->
+            <p><?= $_SERVER['SERVER_NAME']; ?></p>
+        </p>
+    </body>
+</html>
